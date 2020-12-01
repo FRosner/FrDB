722a95d8885db7cde0cff9ed54dbb0ec57f5ac5c
Frank Rosner <frank@fam-rosner.de>
2020-12-01 12:49:32 +0100

    simplify test

--- repl/help_test.go
+++ repl/help_test.go
@@ -1,23 +1,16 @@
 package repl_test
 
 import (
+	"bytes"
 	"github.com/frosner/frdb/repl"
 	"github.com/stretchr/testify/assert"
-	"io/ioutil"
-	"os"
 	"testing"
 )
 
 func TestHelp(t *testing.T) {
-	output, err := ioutil.TempFile("", "output-*")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer output.Close()
-	defer os.Remove(output.Name())
-	repl.Help(output)
-	data, err := ioutil.ReadFile(output.Name())
-	assert.Equal(t, string(data), `Available commands:
+	var out bytes.Buffer
+	repl.Help(&out)
+	assert.Equal(t, out.String(), `Available commands:
   - help: shows this text
   - exit: exits the REPL
 `)
